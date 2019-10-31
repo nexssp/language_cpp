897a1ff0a3b8c58aae2241f30831218783efd7c5
Marcin Polak <mapoart@gmail.com>
2019-10-31 14:11:34 +0100

    init for c++

---
+++ templates/_helloWorld.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <string>
+#include "rapidjson/document.h"
+#include "rapidjson/pointer.h"
+#include "rapidjson/stringbuffer.h"
+#include <rapidjson/writer.h>
+
+using namespace std;
+using namespace rapidjson;
+
+template <typename T>
+string stringify(const T &o)
+{
+    StringBuffer sb;
+    Writer<StringBuffer> writer(sb);
+    o.Accept(writer);
+    return sb.GetString();
+}
+
+int main()
+{
+    Document document;
+    StringBuffer sb;
+    // Document::AllocatorType &allocator = document.GetAllocator();
+
+    for (std::string line; std::getline(std::cin, line);)
+    {
+
+        document.Parse<0>(line.c_str());
+        //document["start"] = 1000000000;
+        // FIXME: fix the printf to format string.. bow output is wrong __cplusplus_
+        Pointer("/outputCPP").Set(document, "Hello from C++ 17!");
+        // document.AddMember("outputCPP", "Hello from C++! ");
+
+        std::cout << stringify(document) << std::endl;
+    }
+    return 0;
+}
