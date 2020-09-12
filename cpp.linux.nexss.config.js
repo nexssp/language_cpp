72577a1692346dfa0582707acf1dd5e9b2b86234
Marcin Polak <mapoart@gmail.com>
2020-09-12 20:33:26 +0200

    Another changes for linux distros (Oracle Linux)

--- cpp.linux.nexss.config.js
+++ cpp.linux.nexss.config.js
@@ -3,9 +3,19 @@ let sudo = "sudo ";
 if (process.getuid && process.getuid() === 0) {
   sudo = "";
 }
+
+const {
+  replaceCommandByDist,
+  dist,
+  version,
+} = require(`${process.env.NEXSS_SRC_PATH}/lib/osys`);
+
+const distName = dist();
+languageConfig.dist = distName;
+
 languageConfig.builders = {
   gcc: {
-    install: `${sudo}apt install -y gcc`,
+    install: replaceCommandByDist(`${sudo}apt install -y gcc`),
     command: "g++",
     build: () => {
       return "g++";
@@ -14,11 +24,52 @@ languageConfig.builders = {
     help: ``,
   },
   "g++": {
-    install: `${sudo}apt install -y llvm`,
+    install: replaceCommandByDist(`${sudo}apt install -y llvm`),
     command: "g++",
     args: "-o <file>.exe && <file>.exe && rm <file>.exe",
     help: ``,
   },
 };
+
+if (distName === "Ubuntu" || distName === "Debian") {
+  languageConfig.builders.gcc.install += " build-essential";
+  languageConfig.builders["g++"].install += " build-essential";
+} else if (distName === "Oracle Linux Server") {
+  languageConfig.builders.gcc.install += " gcc-c++";
+  languageConfig.builders["g++"].install += " gcc-c++";
+}
+
+switch (
+  distName
+  // case "Oracle":
+  // case "Oracle Linux Server":
+  //   if (version) {
+  //     //if here for older versions of nexssp
+  //     const distVersion = version(); // *1 converts to number
+  //     if (distVersion >= 8) {
+  //       // TODO: recognize the slim version
+  //       languageConfig.builders.gcc.install = `dnf update -y && ${sudo}dnf install -y php php-json php-imap`;
+  //     } else {
+  //       languageConfig.builders.gcc.install = `${sudo}yum update -y && ${sudo}yum install -y oracle-epel-release* && ${sudo}yum install -y php php-json php-imap`;
+  //     }
+  //   }
+  //   break;
+  // case "Alpine Linux":
+  //   languageConfig.builders.gcc.install = `${sudo}apk add php php7-json php-imap`;
+  //   break;
+  // case "Arch Linux":
+  //   languageConfig.builders.gcc.install = `${sudo}pacman -Sy --noconfirm php php-imap`;
+  //   break;
+  // case "Fedora":
+  //   languageConfig.builders.gcc.install = `${sudo}dnf install -y php php-json php-imap`;
+  //   break;
+  // case "CentOS Linux":
+  //   languageConfig.builders.gcc.install = `${sudo}yum install -y epel-release* && yum install -y php php-json php-imap`;
+  //   break;
+  // case "RHEL Linux":
+  //   languageConfig.builders.gcc.install = `${sudo}yum install -y php php-json php-imap`;
+  //   break;
+) {
+}
 
 module.exports = languageConfig;
