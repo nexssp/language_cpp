b6fb0bad40a33489d8da3a0781270dfa644f1164
Marcin Polak <mapoart@gmail.com>
2020-09-29 09:58:13 +0200

    Fixes for few distros

--- cpp.linux.nexss.config.js
+++ cpp.linux.nexss.config.js
@@ -5,7 +5,7 @@ const distName = os.name();
 languageConfig.dist = distName;
 
 const vcpkgInstall = `echo Installing package manager vcpkg..
-git clone https://github.com/microsoft/vcpkg ${process.env.NEXSS_APPS_PATH}/vcpkg
+if [ ! -d ${process.env.NEXSS_APPS_PATH}/vcpkg ] ; then git clone https://github.com/microsoft/vcpkg ${process.env.NEXSS_APPS_PATH}/vcpkg ; fi
 ${sudo}apt-get install curl unzip tar cmake
 ${process.env.NEXSS_APPS_PATH}/vcpkg/bootstrap-vcpkg.sh
 cd ${process.env.NEXSS_APPS_PATH}/vcpkg
@@ -13,8 +13,8 @@ ${sudo}ln -s $(pwd)/vcpkg /usr/bin/vcpkg`;
 
 languageConfig.builders = {
   gcc: {
-    install: os.replacePMByDistro(`${sudo}apt install -y gcc cmake
-${vcpkgInstall}`),
+    install: `${sudo}apt install -y gcc cmake
+${vcpkgInstall}`,
     command: "g++",
     build: () => {
       return "g++";
@@ -23,8 +23,8 @@ ${vcpkgInstall}`),
     help: ``,
   },
   "g++": {
-    install: os.replacePMByDistro(`${sudo}apt install -y llvm
-${vcpkgInstall}`),
+    install: `${sudo}apt install -y llvm
+${vcpkgInstall}`,
     command: "g++",
     args: "-o <file>.exe && <file>.exe && rm <file>.exe",
     help: ``,
@@ -35,22 +35,32 @@ switch (distName) {
   case os.distros.UBUNTU:
   case os.distros.DEBIAN:
     languageConfig.builders.gcc.install += `
-${sudo}apt-get install build-essential g++`;
+${sudo}apt-get install -y build-essential g++`;
     languageConfig.builders["g++"].install += `
-${sudo}apt-get install build-essential g++`;
+${sudo}apt-get install -y build-essential g++`;
     break;
   case os.distros.ORACLE:
     languageConfig.builders.gcc.install += `
-${sudo}apt-get install gcc-c++`;
+${sudo}apt-get install -y gcc-c++ cmake make ncurses-devel`;
     languageConfig.builders["g++"].install += `
-${sudo}apt-get install gcc-c++`;
+${sudo}apt-get install -y gcc-c++`;
+    break;
   case os.distros.ARCH:
     languageConfig.builders.gcc.install += `
-${sudo}apt-get install gcc-c++`;
+${sudo}apt-get install -y gcc-c++`;
     languageConfig.builders["g++"].install += `
-${sudo}apt-get install gcc-c++`;
+${sudo}apt-get install -y gcc-c++`;
+    break;
   default:
     break;
 }
+
+languageConfig.builders.gcc.install = os.replacePMByDistro(
+  languageConfig.builders.gcc.install
+);
+
+languageConfig.builders["g++"].install = os.replacePMByDistro(
+  languageConfig.builders["g++"].install
+);
 
 module.exports = languageConfig;
