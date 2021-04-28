44a0e2bdf1740a1d4c4f465aa10596e992a7875c
Marcin Polak <mapoart@gmail.com>
2021-04-28 02:12:33 +0200

    removed unnecessary modules

--- install/installVCPKG.js
+++ install/installVCPKG.js
@@ -12,8 +12,7 @@ if ! command -v git &> /dev/null; then ${sudo}apt-get install -y git; fi
 if [ ! -d ${process.env.NEXSS_APPS_PATH}/vcpkg ] ; then git clone https://github.com/microsoft/vcpkg ${process.env.NEXSS_APPS_PATH}/vcpkg --depth=1 ; fi
 ${sudo}apt-get install ${extraPackages} curl unzip tar cmake 
 if ! command -v vcpkg &> /dev/null; then ${process.env.NEXSS_APPS_PATH}/vcpkg/bootstrap-vcpkg.sh; fi
-if ! command -v vcpkg &> /dev/null; then cd ${process.env.NEXSS_APPS_PATH}/vcpkg; fi
-if ! command -v vcpkg &> /dev/null; then ${sudo}ln -s $(pwd)/vcpkg /usr/bin/vcpkg; fi
-if ! command -v vcpkg &> /dev/null; then ${sudo}vcpkg integrate install; fi`;
+if ! command -v vcpkg &> /dev/null; then cd ${process.env.NEXSS_APPS_PATH}/vcpkg; ${sudo}ln -s $(pwd)/vcpkg /usr/bin/vcpkg; fi
+${sudo}vcpkg integrate install`;
 
 module.exports = { vcpkgInstall };
